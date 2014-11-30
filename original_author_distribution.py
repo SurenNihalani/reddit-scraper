e01e22afc9089417b946b563f58b4c9fcf1f45f8
Suren Nihalani <suren.k.n@me.com>
2014-11-30 18:17:11 -0500

    Adding original authors distribution

---
+++ original_author_distribution.py
@@ -0,0 +1,32 @@
+__author__ = 'sn'
+import json
+from collections import Counter
+from matplotlib import pyplot
+
+with open('data2.csv', 'r') as f:
+    lengths = []
+    for line in f:
+        line = line.split(',', 6)
+
+        line = line[-1]
+        line = json.loads(line)
+        if len(line) > 0:
+            line.sort(key=lambda s: s[2])
+            original_author = line[0][0]
+            print original_author
+            lengths.append(len([item for item in line if item[0] == original_author]))
+    print lengths
+    c = Counter(lengths)
+
+    x = list(c.elements())
+    print x
+    bins = [i * 1 for i in range(10)]
+
+    pyplot.hist(x, bins=bins, facecolor='green', alpha=0.75)
+    pyplot.xlabel('Number of times original author posted')
+    pyplot.ylabel('Count')
+    # pyplot.suptitle(r'pvalue')
+
+    pyplot.title(r'Distribution of posts by original author')
+    pyplot.grid(True)
+    pyplot.savefig('original_author' + '.png')
