4a4d3a949e588f467a1ee156034b920e7ca33e9e
Suren Nihalani <suren.k.n@me.com>
2014-12-02 17:38:54 -0500

    Wrote a combiner

---
+++ combiner.py
@@ -0,0 +1,26 @@
+__author__ = 'sn'
+import sys
+
+print 'Number of arguments:', len(sys.argv), 'arguments.'
+
+print 'Argument List:', str(sys.argv)
+
+if len(sys.argv) == 1:
+    print "give me some files to combine"
+visited = set()
+
+with open('combined.txt', 'w') as w:
+    for file in sys.argv[1:]:
+        with open(file, 'r') as source:
+            for line in source:
+                full_line = line
+                line = line.strip()
+                line = line.split(',', 6)
+
+                print line
+                row_number = int(line[0])
+                if row_number in visited:
+                    continue
+                visited.add(row_number)
+                w.write(full_line)
+
